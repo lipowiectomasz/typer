292111acbfa66e2891401e94f5152da537aa7882
puscianm <teranowa1995@gmail.com>
2024-05-23 21:37:24 +0200

    Added pgadmin. Unsuccessful table initialization

---
+++ create_tables copy.sql
@@ -0,0 +1,5 @@
+CREATE TABLE IF NOT EXISTS schedule (
+    match_begin_time TIME,
+    host_name VARCHAR(255),
+    guest_name VARCHAR(255)
+);
--- pythonback/pythonback.py
+++ pythonback/pythonback.py
@@ -1,5 +1,7 @@
 import os
 import psycopg2
+import requests
+import pandas as pd
 
 def main():
     conn = psycopg2.connect(
@@ -13,6 +15,9 @@ def main():
     cur.execute('SELECT version();')
     db_version = cur.fetchone()
     print(f"Connected to PostgreSQL database! Version: {db_version[0]}")
+    
+    print("Printing schedule table from postgresql db:")
+    print(pd.read_sql("SELECT * FROM schedule",conn))
     cur.close()
     conn.close()
 
